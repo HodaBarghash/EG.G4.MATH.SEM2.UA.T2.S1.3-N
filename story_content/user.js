2e665ffcd24c31011c31bed35c01ccdd13238aba
HodaBarghash <121548764+HodaBarghash@users.noreply.github.com>
2023-02-02 13:32:38 +0200

    g4

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,26 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6eyK6HyYrLj":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  const url="https://script.google.com/macros/s/AKfycbzlLtNxeJ_60U0EBbaQU8tUsftmQoLaayg2sssNp8rPnWx_nmyTjfRsiAf8fHklYZpS4w/exec"
+
+var player=GetPlayer()
+
+fetch(url,{
+	method:"POST",
+	mode: 'no-cors',
+	cache:'no-cache',
+	headers: {'Content-Type': 'application/json'},
+	redirect:'follow',
+	body: JSON.stringify({q1attempts:player.GetVar("q1attempts"),q1answeredcorrect:player.GetVar("q1answeredcorrect")})
+	}
+	)
 }
 
